43a905e2df92cc73274fb6d23b330062d94c9bfb
Silence <taogu2010@yeah.net>
2020-05-10 22:22:33 +0800

    210

--- README.md
+++ README.md
@@ -62,4 +62,4 @@
 - [x] 1. [127. 单词接龙](https://leetcode-cn.com/problems/word-ladder/) WordLadder.java
 - [x] 2. [200. 岛屿数量](https://leetcode-cn.com/problems/number-of-islands/) NumberIslands.java
 - [x] 3. [207. 课程表](https://leetcode-cn.com/problems/course-schedule/) CourseSchedule.java
-- [ ] 4. [210. 课程表 II](https://leetcode-cn.com/problems/course-schedule-ii/)
+- [x] 4. [210. 课程表 II](https://leetcode-cn.com/problems/course-schedule-ii/) CourseSchedule.java
--- src/main/java/com/silentao/leetcode/graph/CourseSchedule.java
+++ src/main/java/com/silentao/leetcode/graph/CourseSchedule.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 /**
  * @Description
- * 207. 课程表
- * https://leetcode-cn.com/problems/course-schedule/
  * @Author Silence
  * @Date 2020/5/9 22:26
  **/
@@ -13,6 +11,8 @@ public class CourseSchedule {
 
     /**
      * 广度优先遍历
+     * 207. 课程表
+     * https://leetcode-cn.com/problems/course-schedule/
      * @param numCourses
      * @param prerequisites
      * @return
@@ -61,5 +61,54 @@ public class CourseSchedule {
 
         // 等于0表示课程学完了
         return numCourses == 0;
+    }
+
+    /**
+     * 210. 课程表 II
+     * https://leetcode-cn.com/problems/course-schedule-ii/
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] need = new int[numCourses];
+        List<List<Integer>> coursese = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            coursese.add(new ArrayList<>());
+        }
+
+        for (int[] cur : prerequisites) {
+            need[cur[0]]++;
+            coursese.get(cur[1]).add(cur[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (need[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] res = new int[numCourses];
+        int k = 0;
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+
+            res[k++] = pre;
+
+            for (int c : coursese.get(pre)) {
+                if (--need[c] == 0) {
+                    queue.add(c);
+                }
+            }
+        }
+
+        if (numCourses != 0) {
+            return new int[0];
+        }
+
+        return res;
     }
 }
